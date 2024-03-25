6de3949549090545074a8e20c594fee10941bead
MC <chokamoses2@gmail.com>
2024-03-25 13:42:19 +0300

    added ahoy, chartkick, and chart bundle on javascript application file

---
+++ config/importmap.rb
@@ -0,0 +1,11 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "chartkick" # @5.0.1
+pin "Char.bundle", to: "Chart.bundle.js"
+pin "ahoy" # @1.0.1
+pin "os" # @2.0.1
